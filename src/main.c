5a260eb2835d4d3e0194a4fbda029d473841b09c
z22038ys <116726502+z22038ys@users.noreply.github.com>
2022-10-28 23:08:46 +0900

    Commit issue#1
    ・ドメインを見て、Gmailと、そうでない場合を分ける処理を実装
     ・単体testをtest.shに書く
     ・READMEにGmailが対応している旨を記載

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void check_domain(char *mail){
+    char *str = strtok(mail, "@");
+    if(strcmp(*str[1],"gmail.com")==0){
+        printf("これはGmailのメールアドレスです\n");
+        //サニタイズの処理を実装
+    }
+    else{
+        printf("これはその他のメールアドレスです\n");
+        //サニタイズの処理を実装
+    }
+}
 
 int main(int argc, char *argv[]){
     char *string;
     int const SIZE = 100;
     string = (char *)calloc(SIZE,sizeof(char)); // 1byte * SIZE分のメモリを確保
     fprintf(stdout, "%s", argv[1]);
+
+    check_domain(argv[1]);
+
     fprintf(stdout, "%s", string);
     return 0;
 }
+
--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,20 @@
 assert(){
    gcc -Wall -o ELF/main src/main.c
    ./ELF/main $1
+   if [ "$?" -eq "0" ]
+   then
+      echo ">SUCCESS"
+   else 
+      echo ">FAIL"
+   fi
 }
 
 
 assert "email@example.com"
+
+#Gmailのテスト
+assert "email.1@gmail.com"    #SUCCESS 半角英字、数字、ピリオドを使用できる
+assert "email..1@gmail.com"   #FAIL ピリオドは連続して使用してはいけない
+assert ".email1@gmail.com"    #FAIL ユーザー名の最初の文字はASCII文字(a-z)または数字(0-9)にする必要がある
+assert "ema[il1@gmail.com"    #FAIL ユーザー名には＆、=、_、'、-、+、,、<>、を使用することはできない
+assert "email436287489143fofhdsjfkhsjfhfsdhs534435kldfjdl@gmail.com" #FAIL 6文字から30文字の間で設定する必要がある
