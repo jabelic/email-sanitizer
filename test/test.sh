42d3e70277511df06514baf19a81c48d5e3b4893
Jabelic <hibiki990523@gmail.com>
2022-11-01 23:36:19 +0900

    ADD: test cases

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,10 @@
 # !/bin/zsh
 
 assert(){
-   gcc -Wall -o ../ELF/main ../src/main.c
-   ../ELF/main $1
+   # gcc -Wall -o ../ELF/main ../src/main.c
+   # ../ELF/main $1
+   gcc -Wall -o ELF/main src/main.c
+   ELF/main $1
 
    if [ "$?" -eq "0" ]
    then
@@ -24,3 +26,14 @@ assert "ema=il1@gmail.com"    #FAIL ユーザー名には＆、=、_、'、-、+
 assert "12345>@gmail.com"     #FAIL ユーザー名には＆、=、_、'、-、+、,、<>、を使用することはできない
 assert "12345@gmail.com"      #FAIL 6文字から30文字の間で設定する必要がある
 assert "email436287489143fofhdsjfkhsjfh@gmail.com" #FAIL 6文字から30文字の間で設定する必要がある
+
+
+#yahooメール
+assert "hoge@yahoo.co.jp"      #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし。
+assert "hogE@yahoo.co.jp"      #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし。
+assert "hoge_huga@yahoo.co.jp" #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし。
+assert "10_23hoge@yahoo.co.jp" #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし
+assert "hoge_@yahoo.co.jp"     #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし
+assert "1hoge@yahoo.co.jp"     #FAIL     最初の文字は数字、記号ではなく必ずアルファベットにする。
+assert "tmp@yahoo.co.jp"       #FAIL     ローカル部(ID)は4～31文字で構成される。
+assert "t1p@yahoo.co.jp"       #FAIL     ローカル部(ID)は4～31文字で構成される。
