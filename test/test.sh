b8c8e5c7f33e1c1899a2313ba5c404f2d282a8e2
Jabelic <hibiki990523@gmail.com>
2022-11-02 02:12:34 +0900

    ADD: test cases for ymail.ne.jp

--- test/test.sh
+++ test/test.sh
@@ -28,12 +28,32 @@ assert "12345@gmail.com"      #FAIL 6文字から30文字の間で設定する�
 assert "email436287489143fofhdsjfkhsjfh@gmail.com" #FAIL 6文字から30文字の間で設定する必要がある
 
 
-#yahooメール
-assert "hoge@yahoo.co.jp"      #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし。
-assert "hogE@yahoo.co.jp"      #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし。
-assert "hoge_huga@yahoo.co.jp" #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし。
-assert "10_23hoge@yahoo.co.jp" #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし
-assert "hoge_@yahoo.co.jp"     #SUCCESS  4～31文字の半角英数字、記号（アンダーバー「_」のみ）が使える。大文字小文字の区別なし
-assert "1hoge@yahoo.co.jp"     #FAIL     最初の文字は数字、記号ではなく必ずアルファベットにする。
+# yahoo.co.jp
+# 4～31文字の半角英数字、記号（ _ ）が使える。先頭の文字は必ず英字。
+# おそらく末尾に記号（ _ ）は使えない
+assert "hoge@yahoo.co.jp"      #SUCCESS  
+assert "hogE@yahoo.co.jp"      #SUCCESS  
+assert "hoge_123_huga@yahoo.co.jp" #SUCCESS  
+assert "email436287489143fofhdsjfkhsjfhA@yahoo.co.jp"     #FAIL  ローカル部(ID)は4～31文字で構成される。
 assert "tmp@yahoo.co.jp"       #FAIL     ローカル部(ID)は4～31文字で構成される。
 assert "t1p@yahoo.co.jp"       #FAIL     ローカル部(ID)は4～31文字で構成される。
+assert "1hoge@yahoo.co.jp"     #FAIL     最初の文字は数字、記号ではなく必ずアルファベットにする。
+# assert "hoge_@yahoo.co.jp"     #FAIL?
+
+# ymail.ne.jp
+# 使用可能な文字：半角小文字英数字、「\_」（アンダーバー)、「.」(ドット)、「-」（ハイフン）
+# 文字列の長さ：3 文字〜29 文字
+# 先頭の文字は半角小文字英数字のみ使用できます
+# 「.」(ドット)の末尾での使用や、連続使用はできません
+assert "hog@ymail.ne.jp"                     #SUCCESS
+assert "hoge-huga.123@ymail.ne.jp"           #SUCCESS
+assert "hoge_huga-123.piyo@ymail.ne.jp"      #SUCCESS
+assert "hoge_huga-123-@ymail.ne.jp"          #SUCCESS
+assert "hoge_huga-123_@ymail.ne.jp"          #SUCCESS
+assert "hoge_huga-123.@ymail.ne.jp"          #FAIL  「.」(ドット)の末尾での使用や、連続使用はできません
+assert "hoge_huga1..23@ymail.ne.jp"          #FAIL  「.」(ドット)の末尾での使用や、連続使用はできません
+assert "_huga-123@ymail.ne.jp"               #FAIL  先頭の文字は半角小文字英数字のみ使用できます
+assert "-huga_123@ymail.ne.jp"               #FAIL  先頭の文字は半角小文字英数字のみ使用できます
+assert ".huga_123@ymail.ne.jp"               #FAIL  先頭の文字は半角小文字英数字のみ使用できます
+
+assert "hoge@yahoo.ne.jp"      #FAIL Y!mobileには非対応
